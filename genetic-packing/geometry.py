10e06e35cd9af4725bd83d11c4c3a1649669b650
Rutger Bezema <rutger.bezema@rewe-digital.com>
2020-03-11 13:10:32 +0100

    Working on evalutional packing

---
+++ genetic-packing/geometry.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+
+class Point:
+    def __init__(self, coordinates):
+        self.coords = coordinates
+
+    def squared_distance_from(self, other):
+        distance = self.coords - other.coords
+        return sum(distance * distance)
+
+    def scalar_less_than(self, other):
+        return max(self.coords - other.coords) <= 0
+
+    def maximum(self, other):
+        return Point(np.maximum(self.coords, other.coords))
+
+    def minimum(self, other):
+        return Point(np.minimum(self.coords, other.coords))
+
+    def prod(self):
+        return np.prod(self.coords)
+
+    def __add__(self, other):
+        return Point(self.coords + other.coords)
+
+    def __sub__(self, other):
+        return Point(self.coords - other.coords)
+
+    def __str__(self):
+        return str(self.coords)
+
+
+class Cuboid:
+    def __init__(self, dimensions):
+        self.dimensions = dimensions
+
+    def volume(self):
+        return self.dimensions.prod()
+
+    def can_fit_in(self, space) -> bool:
+        return min(space.dimensions() - self.dimensions) >= 0
+        # space.width() >= self.width && space.height() >= self.height && space.depth() >= self.depth
+
+    def __str__(self):
+        return str(self.dimensions)
+
+
+class Space:
+    def __init__(self, bottom_left: Point, upper_right: Point):
+        if (min(upper_right - bottom_left) < 0):
+            raise ValueError(
+                "bottom left " + str(bottom_left) + " must be smaller then upperright: " + str(upper_right))
+        self.bottom_left = bottom_left
+        self.upper_right = upper_right
+
+    @staticmethod
+    def from_placement(origin: Point, rect: Cuboid):
+        return Space(origin, origin + rect.dimensions)
+
+    def dimensions(self):
+        return self.upper_right - self.bottom_left
+
+    def origin(self) -> Point:
+        return self.bottom_left
+
+    def center(self) -> Point:
+        return Point((self.bottom_left + self.upper_right) / 2.0)
+
+    # pub fn width(&self) -> i32 {
+    #    self.upper_right.x - self.bottom_left.x
+    # }
+
+    # pub fn depth(&self) -> i32 {
+    #    self.upper_right.z - self.bottom_left.z
+    # }
+
+    # pub fn height(&self) -> i32 {
+    #    self.upper_right.y - self.bottom_left.y
+    # }
+
+    def contains(self, other) -> bool:
+        return self.bottom_left.scalar_less_than(other.bottom_left) and \
+               other.upper_right.scalar_less_than(self.upper_right)
+
+    def intersects(self, other) -> bool:
+        return self.bottom_left.scalar_less_than(other.upper_right) and \
+               other.bottom_left.scalar_less_than(self.upper_right)
+
+    def union(self, other):
+        return Space(self.bottom_left.maximum(other.bottom_left), self.upper_right.minimum(other.upper_right))
+
+    def volume(self) -> float:
+        return self.dimensions().prod()
+
+    def __str__(self):
+        return str(self.bottom_left) + ", " + str(self.upper_right)
+
+
+if __name__ == '__main__':
+    a = Point(np.array([3, 7, 3]))
+    b = Point(np.array([5, 5, 5]))
+    print(a)
+    print(b)
+    print(a.scalar_less_than(b))
+    print(a.squared_distance_from(b))
+    print(a - b)
+    print(a + b)
+    print(a.minimum(b))
+    print(a.maximum(b))
+    print(a.prod())
+    print(b.prod())
+
+    c = Cuboid(Point(np.array([3, 5, 7])))
+    print(c)
+    print(c.volume())
+
+    d = Point(np.array([3, 2, 3]))
+    s = Space(d, b)
+    print(s)
+    s2 = Space.from_placement(d, c)
+    print(s2)
+
+    print(s.dimensions())
+    print(s.origin())
+    print(s.center())
--- knapsack/ant_momkp.py
+++ knapsack/ant_momkp.py
@@ -9,7 +9,49 @@ def lambda_weight_vector(m, number_of_weight_vectors, g, weight_change_frequency
     )
     return [lambda_1, 1-lambda_1]
 
+
+def initialize_pheromone_trails(trails=1, generation=0):
+    pass
+
+
+def generate_weight_functions(size_of_weightfunction):
+    return []
+
+
+def explore_pareto_space(number_of_generations):
+    generation: int = 0
+    while generation < number_of_generations:
+        # 1) Archive initialization: A ← ∅
+        # 2) Weight vector selection: Select weight vector λ(g)=(λ1(g),...,λm(g)) ∈ Λ
+        # 3) Solutions construction: For h from 1 to Nbants: Ant h constructs a solution Sh
+        # with probability pSh (Ij ) defined in Eq.(8)
+        # 4) Archive update: If no solution in A  Sh add Sh to A and remove from A all
+        # solutions dominated by Sh
+        # 5) Pheromone update: Update pheromone trails for each member of A with τ(Ij )
+        # defined in Eq.(11)
+        # 6) Pareto approximation set update: P ← non-dominated solutions of P ∪ A
+        # 7) Generation index incrementation: g ← g + 1
+
+        generation +=1
+
+
 def main():
+    # Input: Nbants (number of ants)
+    #gmax (maximum number of generations)
+    # m (number of objectives)
+    # Output: P (Pareto approximation set)
+    # Step 1 - initialization: Initialize the pheromone trails τinit ← 1 and g ← 0
+    # Step2 -  weight vector generation: Generate the weight vectors set Λ of size L according to Gw method
+    # Step 3 - main loop: While g < gmax do: explore
+    # Step 4-Termination: Return the Pareto approximation set P
+    number_of_generations: int = 200
+    number_of_objectives: int = 2
+    number_of_ants: int = 100
+    number_of_items: int = 10
+    size_of_weightfunction = 10
+    initialize_pheromone_trails(1, 0)
+    weight_functions = generate_weight_functions(size_of_weightfunction)
+    explore_pareto_space()
 
 
 
