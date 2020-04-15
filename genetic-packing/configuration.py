8d8d90a5947146fb49143e4f1077a556cf4f7906
Rutger <rbezema@gmx.net>
2020-04-15 16:20:01 +0200

    Setup a rosnode actionserver

--- genetic-packing/configuration.py
+++ genetic-packing/configuration.py
@@ -1,6 +1,13 @@
+import numpy as np
+
+from geometry import Cuboid, Point
+
+
 class Parameters:
     def __init__(self, population_factor=30, elites_percentage=0.1, mutants_percentage=0.15,
-                 inherit_elite_probability=0.7, max_generations=200, max_generations_no_improvement=5, number_of_threads=10):
+                 inherit_elite_probability=0.7, max_generations=200, max_generations_no_improvement=5,
+                 number_of_threads=10,
+                 delivery_bin_spec=None):
         self.population_factor = population_factor
         self.elites_percentage = elites_percentage
         self.mutants_percentage = mutants_percentage
@@ -8,6 +15,9 @@ class Parameters:
         self.max_generations = max_generations
         self.max_generations_no_improvement = max_generations_no_improvement
         self.number_of_threads = number_of_threads
+        if delivery_bin_spec is None:
+            delivery_bin_spec = [30, 30, 30]
+        self.delivery_bin_spec = Cuboid(Point(np.array(delivery_bin_spec)))
 
     @staticmethod
     def from_yaml(config):
@@ -18,7 +28,8 @@ class Parameters:
             config['inherit_elite_probability'],
             config['max_generations'],
             config['max_generations_no_improvement'],
-            config['number_of_threads']
+            config['number_of_threads'],
+            config['delivery_bin_spec']
         )
 
     def get_ga_params(self, num_items: int):
@@ -32,24 +43,34 @@ class Parameters:
             inherit_elite_probability=self.inherit_elite_probability,
             max_generations=self.max_generations,
             max_generations_no_improvement=self.max_generations_no_improvement,
-            number_of_threads=self.number_of_threads
+            number_of_threads=self.number_of_threads,
+            delivery_bin_spec=self.delivery_bin_spec
         )
 
     def __repr__(self):
-        return "(population_factor=%r, elites_percentage=%r, mutants_percentage=%r, inherit_elite_probability=%r, max_generations=%r, max_generations_no_improvement=%r, number_of_threads=%r)" % (
-            self.population_factor,
-            self.elites_percentage,
-            self.mutants_percentage,
-            self.inherit_elite_probability,
-            self.max_generations,
-            self.max_generations_no_improvement,
-            self.number_of_threads
-        )
+        return "(population_factor=%r, " \
+               "elites_percentage=%r, " \
+               "mutants_percentage=%r, " \
+               "inherit_elite_probability=%r, " \
+               "max_generations=%r, " \
+               "max_generations_no_improvement=%r, " \
+               "number_of_threads=%r, " \
+               "delivery_bin_spec=%r" \
+               ")" % (
+                   self.population_factor,
+                   self.elites_percentage,
+                   self.mutants_percentage,
+                   self.inherit_elite_probability,
+                   self.max_generations,
+                   self.max_generations_no_improvement,
+                   self.number_of_threads,
+                   self.delivery_bin_spec
+               )
 
 
-class GAParameters():
+class GAParameters:
     def __init__(self, population_size, num_elites, num_mutants, inherit_elite_probability, max_generations,
-                 max_generations_no_improvement, number_of_threads):
+                 max_generations_no_improvement, number_of_threads, delivery_bin_spec):
         self.max_generations = max_generations
         self.inherit_elite_probability = inherit_elite_probability
         self.max_generations_no_improvement = max_generations_no_improvement
@@ -57,3 +78,4 @@ class GAParameters():
         self.num_elites = num_elites
         self.population_size = population_size
         self.number_of_threads = number_of_threads
+        self.delivery_bin_spec = delivery_bin_spec
